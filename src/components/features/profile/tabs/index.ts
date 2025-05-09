f03e37b299ba05a6c781ebaeb1a1d433377ea57d
Askar Uvaliev <a@uvaliyev.com>
2025-05-09 14:12:25 +0500

    refactor: remove unused files and legacy components

--- src/components/features/profile/tabs/index.ts
+++
@@ -1,5 +0,0 @@
-export { ProfileTab } from './ProfileTab';
-export { NotificationsTab } from './NotificationsTab';
-export { AccountTab } from './AccountTab';
-export { PasswordTab } from './PasswordTab';
-export { BillingTab } from './BillingTab'; 
