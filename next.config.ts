0d39aa95cce01263bf9d3e09b49f8d4d28cb779c
Askar Uvaliev <a@uvaliyev.com>
2025-05-28 16:38:34 +0500

    chore: update dependencies and remove environment template

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh'],
+    domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh', '15fed70ebe1d7c50715513ab6046afe2.r2.cloudflarestorage.com'],
   },
 };
 
