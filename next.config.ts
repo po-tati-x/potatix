e8f2bf36f690dd338ccbfb8c98fb05ed85b05ee5
Askar Uvaliev <a@uvaliyev.com>
2025-05-31 14:42:44 +0500

    fix(images): replace deprecated domains with remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['avatars.githubusercontent.com', 'avatar.vercel.sh', '15fed70ebe1d7c50715513ab6046afe2.r2.cloudflarestorage.com', 'images.unsplash.com', 'storage.potatix.com', 'image.mux.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'avatar.vercel.sh' },
+      { protocol: 'https', hostname: '15fed70ebe1d7c50715513ab6046afe2.r2.cloudflarestorage.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'storage.potatix.com' },
+      { protocol: 'https', hostname: 'image.mux.com' },
+    ],
   },
   devIndicators: false
 };
