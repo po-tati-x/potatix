71f4bbcc1dcb1c8672d325b26abc1ccda29d1609
Askar Uvaliev <a@uvaliyev.com>
2025-06-03 18:40:15 +0500

    refactor(layout): reorganize layout components into dedicated directory structure

--- src/components/layout/sidebar/main-nav.tsx
+++ src/components/layout/sidebar/main-nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils/cn";
 import { usePathname } from "next/navigation";
 import {
   ComponentType,
--- src/components/layout/sidebar/sidebar-components.tsx
+++ src/components/layout/sidebar/sidebar-components.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { ChevronLeft, ChevronDown } from 'lucide-react';
-import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils/cn';
 import { NavItem, NavSection, useNavItemActive } from './sidebar-navigation';
 
 // Sidebar header component
