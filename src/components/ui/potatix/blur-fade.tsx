34d598fa329b8f8e6abb10481d6cf6f910d9d318
Askar Uvaliev <a@uvaliyev.com>
2025-06-05 13:31:49 +0500

    feat(auth): add testimonial carousel and blur fade components

---
+++ src/components/ui/potatix/blur-fade.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import {
+  AnimatePresence,
+  motion,
+  useInView,
+  UseInViewOptions,
+  Variants,
+  MotionProps,
+} from "motion/react";
+import { useRef } from "react";
+
+type MarginType = UseInViewOptions["margin"];
+
+interface BlurFadeProps extends MotionProps {
+  children: React.ReactNode;
+  className?: string;
+  variant?: {
+    hidden: { y: number };
+    visible: { y: number };
+  };
+  duration?: number;
+  delay?: number;
+  offset?: number;
+  direction?: "up" | "down" | "left" | "right";
+  inView?: boolean;
+  inViewMargin?: MarginType;
+  blur?: string;
+}
+
+export function BlurFade({
+  children,
+  className,
+  variant,
+  duration = 0.4,
+  delay = 0,
+  offset = 6,
+  direction = "down",
+  inView = false,
+  inViewMargin = "-50px",
+  blur = "6px",
+  ...props
+}: BlurFadeProps) {
+  const ref = useRef(null);
+  const inViewResult = useInView(ref, { once: true, margin: inViewMargin });
+  const isInView = !inView || inViewResult;
+  const defaultVariants: Variants = {
+    hidden: {
+      [direction === "left" || direction === "right" ? "x" : "y"]:
+        direction === "right" || direction === "down" ? -offset : offset,
+      opacity: 0,
+      filter: `blur(${blur})`,
+    },
+    visible: {
+      [direction === "left" || direction === "right" ? "x" : "y"]: 0,
+      opacity: 1,
+      filter: `blur(0px)`,
+    },
+  };
+  const combinedVariants = variant || defaultVariants;
+  return (
+    <AnimatePresence>
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        exit="hidden"
+        variants={combinedVariants}
+        transition={{
+          delay: 0.04 + delay,
+          duration,
+          ease: "easeOut",
+        }}
+        className={className}
+        {...props}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+
+/*
+
+EXAMPLE USAGE:
+
+import { BlurFade } from "@/components/magicui/blur-fade";
+
+export function BlurFadeTextDemo() {
+  return (
+    <section id="header">
+      <BlurFade delay={0.25} inView>
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+          Hello World 👋
+        </h2>
+      </BlurFade>
+      <BlurFade delay={0.25 * 2} inView>
+        <span className="text-pretty text-xl tracking-tighter sm:text-3xl xl:text-4xl/none">
+          Nice to meet you
+        </span>
+      </BlurFade>
+    </section>
+  );
+}
+
+*/
