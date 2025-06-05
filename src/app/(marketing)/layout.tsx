219d1e21834ce18a22bd10bbb6eb4df3f7103a02
Askar Uvaliev <a@uvaliyev.com>
2025-06-05 22:11:52 +0500

    feat(layout): implement blur effects in marketing layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,9 +1,20 @@
 import Header from '@/components/features/marketing/layout/navbar';
 import Footer from '@/components/features/marketing/layout/footer';
+import { BottomBlur } from '@/components/ui/potatix/bottom-blur';
+import { TopBlur } from '@/components/ui/potatix/top-blur';
+import { Roboto_Flex } from 'next/font/google';
+
+const roboto = Roboto_Flex({
+  subsets: ['latin'],
+  weight: ['500'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className={`${roboto.variable} flex min-h-screen flex-col`}>
+      <TopBlur />
       <Header />
       <main className="flex-1 w-full">
         {children}
@@ -11,6 +22,7 @@ export default function MarketingLayout({ children }: { children: React.ReactNod
       <footer className="w-full bg-gray-900">
         <Footer />
       </footer>
+      <BottomBlur />
     </div>
   );
 }
