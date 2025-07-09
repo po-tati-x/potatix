63f5bc3307977b19b97496d95972d0293834ced1
Askar Uvaliev <a@uvaliyev.com>
2025-07-09 16:17:11 -0700

    chore(types): add pragmatic drag-and-drop type definitions to support new nav components

---
+++ apps/web/src/types/pragmatic.d.ts
@@ -0,0 +1,3 @@
+declare module '@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge';
+declare module '@atlaskit/pragmatic-drag-and-drop-hitbox/util/get-reorder-destination-index';
+declare module '@atlaskit/pragmatic-drag-and-drop-react-drop-indicator/box'; 
