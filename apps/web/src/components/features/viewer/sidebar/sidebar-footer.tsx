80fd4f9998abfd33251596308c2196598c149336
Askar Uvaliev <a@uvaliyev.com>
2025-07-17 01:04:48 -0700

    style(sidebar): copy & spacing tweak in footer

--- apps/web/src/components/features/viewer/sidebar/sidebar-footer.tsx
+++ apps/web/src/components/features/viewer/sidebar/sidebar-footer.tsx
@@ -7,9 +7,8 @@ function SidebarFooter() {
   return (
     <div className="p-4 border-t border-slate-200 bg-slate-50">
       <div className="p-3 bg-emerald-50 border border-emerald-100 rounded-md">
-        <p className="text-xs text-slate-700 mb-2">
-          Potatix is a modern and performance oriented software built with LOVE in SF, <Link className="underline" href="https://hackclub.com">Hackclub</Link>
-        </p>
+        <p className="text-xs text-slate-700">
+          Potatix is a performance oriented LMS</p>
       </div>
     </div>
   );
